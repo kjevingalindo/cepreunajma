2d3a921d4709395d8fda85ed4dbae6ec701a879f
kjevingalindo <96062132+kjevingalindo@users.noreply.github.com>
2025-02-11 13:46:32 -0500

    navbar
    logo cepre

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
 // components/Navbar.tsx
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav className="bg-orange-600 shadow-lg fixed w-full top-0 z-50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+      <Image
+            src="/images/logo-cepre.png" // Ruta de la imagen
+            alt="Logo CEPRE"
+            width={100} // Ancho de la imagen
+            height={100} // Alto de la imagen
+            className="mr-2" // Margen a la derecha del texto
+          />
         <Link href="/" className="text-xl font-bold text-white">
           CEPRE UNAJMA
         </Link>
