05a03f98704e4c3c87a948b929eb3baf52384e39
kjevingalindo <96062132+kjevingalindo@users.noreply.github.com>
2025-02-11 15:34:25 -0500

    ha
    corecc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             </p>
             <Link
               href="/carreras"
-              className="bg-orange-500 text-white px-6 py-3 rounded-lg hover:bg-orange-600 transition duration-300"
+              className="bg-orange-500 text-white px-6 py-3 rounded-lg hover:bg-orange-700 transition duration-300 shadow-lg"
             >
               Explora Nuestras Carreras
             </Link>
